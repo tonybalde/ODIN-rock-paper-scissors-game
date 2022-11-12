b3154a6c2ccc18bc3c10fd9f1e986f75aaf1d263
Tony Baldessari <tonybalde@hotmail.com>
2022-11-11 21:13:05 -0300

    first version working

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ function getComputerChoice(){
 };  // function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 
 
-// var computerSelection = getComputerChoice();
+var computerSelection = getComputerChoice();
 
 // var playerSelection = prompt("Choose: 'Rock', 'Paper' or 'Scissors': ");
-
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters:
 // - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so:
 // "You Lose! Paper beats Rock"
 // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
 // playerSelection = playerSelection.toLowerCase();
-// computerSelection = computerSelection.toLowerCase();
+
+computerSelection = computerSelection.toLowerCase();
 
 var playerWins = 0;
 var computerWins = 0;
@@ -27,60 +26,101 @@ var wins = 5;
 var round = 0;
 var ties = 0;
 
+let roundMarker = document.getElementById('round-marker');
+let userScore = document.getElementById('user_score');
+let pcScore = document.getElementById('pc_score');
+let roundWinnder = document.getElementById('round-winner-marker');
+
+const newDiv = document.createElement("div");
+newDiv.className = "final_msg";
+document.body.appendChild(newDiv);
+const playAgainBtn = document.createElement("button");
+playAgainBtn.className = "play_again_btn";
+playAgainBtn.addEventListener('click', () => location.reload());
+
+var playerSelection = "";
+
+const buttonRock = document.getElementById('btn-r');
+const buttonPaper = document.getElementById('btn-p');
+const buttonScissors = document.getElementById('btn-s');
+
+buttonRock.addEventListener('click', funRock);
+buttonPaper.addEventListener('click', funPaper);
+buttonScissors.addEventListener('click', funScissors);
+
+function funRock(){
+    playerSelection = 'rock';
+    game();
+}
+
+function funPaper(){
+    playerSelection = 'paper';
+    game();
+}
+
+function funScissors(){
+    playerSelection = 'scissors';
+    game();
+}
+
+
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         ties++;
-        alert ("Game Tie");  // change the alert's for return's later on css
     } else if (playerSelection == 'rock' && computerSelection == 'paper'){
         computerWins++;
-        alert("You Lose! Paper beats Rock");
+        pcScore.innerHTML = computerWins;
     } else if (playerSelection == 'rock' && computerSelection == 'scissors'){
         playerWins++;
-        alert("You WIN! Rock beats Scissors");
+        userScore.innerHTML = playerWins;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors'){
         computerWins++;
-        alert("You Lose! Scissors beats Paper");
+        pcScore.innerHTML = computerWins;
     } else if (playerSelection == 'paper' && computerSelection == 'rock'){
         playerWins++;
-        alert("You WIN! Paper beats Rock");
+        userScore.innerHTML = playerWins;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock'){
         computerWins++;
-        alert("You Lose! Rock beats Scissors");
+        pcScore.innerHTML = computerWins;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper'){
         playerWins++;
-        alert("You WIN! Scissors beats Paper");
-        
+        userScore.innerHTML = playerWins;
     } 
 };
 
 function game(){
-    for  (let i = 0; i < 5; i++){
-
-        var computerSelection = getComputerChoice();
-        var playerSelection = prompt("Choose: 'Rock', 'Paper' or 'Scissors': ");
-        playerSelection = playerSelection.toLowerCase();
-        computerSelection = computerSelection.toLowerCase();
-        if (playerSelection == ""){  // solo para ver como va contando
-            alert("You should write Rock, Paper or Scissors");
+        if (playerWins >= 5 || computerWins >= 5) {
+            checkWinner();
+            return;
         } else {
+            var computerSelection = getComputerChoice();
+            computerSelection = computerSelection.toLowerCase();
             playRound(playerSelection, computerSelection);
             round++;
-            console.log("This is the round number: " + round);
+            roundMarker.innerHTML = round;
             
-            
-        }
-        
+            if (playerWins > computerWins){
+                roundWinnder.innerHTML = "PLAYER";
+            } else if (playerWins < computerWins){
+                roundWinnder.innerHTML = "CPU";
+            } else if (playerWins == computerWins){
+                roundWinnder.innerHTML = "TIE";
+
+            }
+            checkWinner();
+        } 
+}       
+
+function checkWinner(){
+    if (playerWins >= 5 && computerWins < 5){
+        newDiv.innerText = "¡You WIN the game!";
+        playAgainBtn.innerText = "Play Again";
+        document.body.appendChild(playAgainBtn);
+    } else if (computerWins >=5 && playerWins < 5){
+        newDiv.innerText = "You LOSE the game!!";
+        playAgainBtn.innerText = "Play Again";
+        document.body.appendChild(playAgainBtn);
     }
-    
-    if (playerWins > computerWins){
-        alert("Player WIN");
-    } else if (playerWins < computerWins){
-        alert("Computer BEATS you!");
-    } else if (playerWins == computerWins){
-        alert("GAME TIE!");
-    }
-    alert("Player Wins: " + playerWins + ", Computer Wins: " + computerWins + ", Games Ties: " + ties);
 }
 
-game();
 
